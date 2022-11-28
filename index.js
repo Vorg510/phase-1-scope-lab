03cad45e23a51f4372b4f22a3069620dd94f3023
Yuferril <yuferril.yunos@gmail.com>
2022-11-28 22:54:22 +1100

    done with lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = 'Paul';
+
+function upperCaseCustomerName() {
+  const ret = customerName.toUpperCase();
+  customerName = ret;
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(name){
+    leastFavoriteCustomer = name;
+
+}
